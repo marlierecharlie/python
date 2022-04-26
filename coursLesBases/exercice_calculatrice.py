f8083bbaebc4d35fb966ef4bab7e1603a1047ff6
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-04-26 10:08:09 +0200

    2e façon de faire

--- coursLesBases/exercice_calculatrice.py
+++ coursLesBases/exercice_calculatrice.py
@@ -1,6 +1,11 @@
-a = input("La somme de ")
-b = input("et de ")
+# a = input("La somme de ")
+# b = input("et de ")
 
-print(f"la somme de {a} et de {b} est égale à {int(a) + int(b)}")
+# print(f"la somme de {a} et de {b} est égale à {int(a) + int(b)}")
 
+
+a = int(input("La somme de "))
+b = int(input("et de "))
+
+print(f"la somme de {a} et de {b} est égale à {a + b}")
  
