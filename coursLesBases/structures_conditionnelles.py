0439c3a10be5d82efe4a063c1a3e339a4092ffd2
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-05-16 22:49:59 +0200

    boucle

--- coursLesBases/structures_conditionnelles.py
+++ coursLesBases/structures_conditionnelles.py
@@ -1,7 +1,9 @@
 # le nombre entré par l'utilisateur est-il + grand que 18? 
 
-age = int(input())
-if age >= 18 :
-    print("vous êtes majeur!")
-elif age < 18 :
-    print("vous êtes mineur")
+# age = int(input())
+# if age >= 18 :
+#     print("vous êtes majeur!")
+# elif age < 18 :
+#     print("vous êtes mineur")
+
+len([1,2,3])
