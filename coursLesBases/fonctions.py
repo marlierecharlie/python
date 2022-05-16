7fc1c54c36c69546614892baa0eb44d593f7d3c7
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-05-16 22:49:21 +0200

    autres boucles

---
+++ coursLesBases/fonctions.py
@@ -0,0 +1,17 @@
+#donne la longueur des élèments ou de la chaîne de caractères
+print(len([1,2]))
+
+#fait l'arrondi
+print(round(1.5))
+
+#donne le minimum et le maximum
+print(min([125,320,7]), max([6,50,2]))
+#ça marche aussi avec les chaines de caractères
+print(min("gateau"))
+
+#fait la somme des élèments
+print(sum([2,3,5]))
+
+#créer une liste de nombre
+for a in range (5) :
+    print (a)
