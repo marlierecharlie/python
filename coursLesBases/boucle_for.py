968ce3cff91d78b0cb7f1f834bb2e0e7f2ab9787
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-05-18 21:46:53 +0200

    boucle for

---
+++ coursLesBases/boucle_for.py
@@ -0,0 +1,13 @@
+#pour parcourir des structures de données
+
+#écrit les nombres de la liste un à un 
+# for i in [1,2,3,4]:
+#     print(i)
+
+#écrit les lettres python 1 à une
+# for lettre in "python":
+#     print(lettre)
+
+#écrit 3 fois "coucou"
+for i in range (3):
+    print("coucou")
