3b8e79e2c40b3c6ff3bde6e29df9aa22f7212f64
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-05-20 22:21:28 +0200

    boucle while

---
+++ coursLesBases/boucle_while.py
@@ -0,0 +1,22 @@
+# i=0 
+# while i < 10:
+#     print ("Bonjour")
+#     i+= 1
+
+# si je ne lui dit pas d'ajouter +1 à i,
+# i reste à 0 et ça fait une boucle infinie
+    
+    
+# continuer = "o"
+# while continuer == "o":
+#     print("on conctinue")
+#     continuer = input ("Voulez vous continuer? o/n ")
+
+#le script s'exécute tant que je clique sur o, si je mets "n" ou 
+#autre chose, il s'arrête
+
+import time 
+while True : 
+    print("Sauvegarde en cours...")
+    time.sleep(10)
+# fait une sauvegarde tt les 10 secondes. 
