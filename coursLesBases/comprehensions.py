384e03185621ab0d1ecd576122c762f9016d1176
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-05-24 17:20:41 +0200

    boucle qui ne fonctionne pas essai 2

--- coursLesBases/comprehensions.py
+++ coursLesBases/comprehensions.py
@@ -1,7 +1,10 @@
-liste = [2, 4, 10, 7]
-# nombres_positifs = []
-# for i in liste : 
-#     if i > 0 : 
-#         nombres_positifs.append(i)
-nombres_positifs = [i for i in liste]
+liste = [-1, -3, 2, 4, 10, 7]
+nombres_positifs = []
+for i in liste :
+    if i > 0 : 
+       print(nombres_positifs.append(i))
+        
+    else :
+        print ("le code marche pas")
+# nombres_positifs = [i for i in liste if i > 0]
 
