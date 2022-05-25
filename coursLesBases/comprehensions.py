0a0429b1d93c300d45a1a53dc3236d71f313d8b6
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-05-25 09:54:09 +0200

    Tentative 3

--- coursLesBases/comprehensions.py
+++ coursLesBases/comprehensions.py
@@ -1,10 +1,9 @@
 liste = [-1, -3, 2, 4, 10, 7]
 nombres_positifs = []
 for i in liste :
-    if i > 0 : 
-       print(nombres_positifs.append(i))
+    if i > 0 : print(nombres_positifs.append(i))
         
-    else :
-        print ("le code marche pas")
-# nombres_positifs = [i for i in liste if i > 0]
+#     else :
+#         print ("le code marche pas")
+# nombres_positifs = [i for i in liste]
 
