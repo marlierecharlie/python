01f798dc36b4d69548af04ddf101ece69ce0405d
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-05-25 10:17:03 +0200

    méthode append test 1

--- coursLesBases/comprehensions.py
+++ coursLesBases/comprehensions.py
@@ -1,8 +1,15 @@
-liste = [-1, -3, 2, 4, 10, 7]
-for i in liste :
-    if i > 0 : print(i)
+# liste = [-1, -3, 2, 4, 10, 7]
+# for i in liste :
+#     if i > 0 : print(i)
         
-    else :
-        continue
+#     else :
+#         continue
 
 
+# autre méthode
+liste = [-1, -3, 2, 4, 10, 7]
+nombres=[]
+for i in liste :
+    if i > 0 : nombres.append(i)
+        
+   
---
+++ coursLesBases/happend.py
@@ -0,0 +1,8 @@
+# pour ajouter un élèments à une liste 
+
+l=[1, 2]
+l.append(56)
+# insert pour pérciser une position
+l.insert(0, 48)
+print(l)
+
