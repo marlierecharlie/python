11c257feb26871032bc416181b293ad1abc7655b
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-05-25 11:53:29 +0200

    problème résolut 2 fois

--- coursLesBases/comprehensions.py
+++ coursLesBases/comprehensions.py
@@ -7,9 +7,16 @@
 
 
 # autre méthode
-liste = [-1, -3, 2, 4, 10, 7]
-nombres=[]
-for i in liste :
-    if i > 0 : nombres.append(i)
+# liste = [-1, -3, 2, 4, 10, 7]
+# nombres=[]
+# for i in liste :
+#     if i > 0 : nombres.append(i)
         
+
+liste = [-5, -4, -3, 0, 1, 6]
+nombres = []
+for i in liste:
+    if i > 0:
+        nombres.append(i)
    
+print(nombres)
