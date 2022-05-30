4370abc5becfc99017a4a21033fff2d74f11a7c9
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-05-30 19:50:29 +0200

    exo 1 et 2

---
+++ coursLesBases/comprehension_exo.py
@@ -0,0 +1,24 @@
+#liste qui réccupère que les nombres pairs (== %2)
+# nombres = [1, 21, 5, 44, 4, 9, 5, 83, 29, 31, 25, 38]
+# nombres_pairs = []
+# for i in nombres :
+#     if i % 2 == 0 :
+#         nombres_pairs.append(i)
+        
+
+
+# nombres_pairs = [i for i in nombres if i % 2 == 0]
+
+# print (nombres_pairs)
+
+#réccupérer uniquement les nombres positifs
+# nombres = range (-10, 10)
+# print (nombres)
+# nombres_positifs = []
+# for i in nombres : 
+#     if i >= 0 : 
+#         nombres_positifs.append(i)
+
+
+# nombres_positifs = [i for i in nombres if i >= 0 ]
+# print (nombres_positifs)
