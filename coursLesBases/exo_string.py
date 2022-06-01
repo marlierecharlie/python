9f50fa18ef848fe144c9679cd2b42a6cf72da8fa
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-06-01 09:26:39 +0200

    2e possibilité

--- coursLesBases/exo_string.py
+++ coursLesBases/exo_string.py
@@ -2,3 +2,10 @@ mot = "python"
 solution = reversed (mot)
 for i in solution:
     print(i)
+    
+# ou
+# on ne peut pas directement utiliser print avec reversed, 
+# il faut aussi utiliser liste c'est comme ça avec plusieurs fonctions de python 3
+# print(list(reversed(mot)))
+for lettre in reversed (mot):
+    print (lettre)
