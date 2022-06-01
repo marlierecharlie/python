471ab7a99f980201241ee17ee2ab2f0acfbb3441
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-06-01 09:12:38 +0200

    2e possibilité

--- coursLesBases/exo_utilisateurs.py
+++ coursLesBases/exo_utilisateurs.py
@@ -1,4 +1,9 @@
 # afficher les utilisateurs de 1 à 10 
 
-for i in range(10):
-    print (f"Utilisateur {i+1}")
+# for i in range(10):
+#     print (f"Utilisateur {i+1}")
+    
+# ou
+
+for i in range(1, 11):
+    print (f"Utilisateur {i}")
