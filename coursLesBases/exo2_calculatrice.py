6e7140ba13123db8bf8db1e5d10227d96f64ac00
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-06-02 15:22:03 +0200

    fonctionne pas pour le moment

---
+++ coursLesBases/exo2_calculatrice.py
@@ -0,0 +1,13 @@
+from curses.ascii import isdigit
+
+
+a = b = ""
+
+While not (a.isdigit() and b.isdigit()) :
+    a = (input("Entrez le premier nombres : "))
+    b = (input("Entrez les deuxième nombre : "))
+    if not (a.isdigit() and b.isdigit()) :
+        print("Veuillez entrer deux nombres valides")
+    
+print(f"le résultat de l'addition {a} avec {b} est égale à {int(a) + int(b)}")
+
