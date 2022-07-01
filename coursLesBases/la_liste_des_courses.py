492f79c5074a856668efcb56d21da2b38efb1eed
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-01 16:14:18 +0200

    création de l'exercice

---
+++ coursLesBases/la_liste_des_courses.py
@@ -0,0 +1,9 @@
+# choisissez parmi les 5 options suivantes : 
+
+# 1 : ajouter un élèment à la liste
+# 2: retirer un élèment à la liste
+# 3: afficher la liste 
+# 4: vider la liste 
+# 5:quitter
+# votre choix:
+
