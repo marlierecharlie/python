4465ff436af09188d38ea3397e885a44973e5377
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-01 16:49:45 +0200

    un peu d'aide

--- coursLesBases/la_liste_des_courses.py
+++ coursLesBases/la_liste_des_courses.py
@@ -1,9 +1,25 @@
-# choisissez parmi les 5 options suivantes : 
+import sys
 
-# 1 : ajouter un élèment à la liste
-# 2: retirer un élèment à la liste
-# 3: afficher la liste 
-# 4: vider la liste 
-# 5:quitter
-# votre choix:
+LISTE=[]
+
+MENU = """ choisissez parmi les 5 options suivantes : 
+
+1 : ajouter un élèment à la liste
+2: retirer un élèment à la liste
+3: afficher la liste 
+4: vider la liste 
+5:quitter
+votre choix: """
+
+Choix_du_menu = ["1", "2", "3", "4", "5"]
+
+# liste_des_courses = []
+
+# # mettre les propositions en ligne 
+
+# input ("choisissez parmi les 5 options suivante : 1 : ajouter un élèment à la liste")
+# if i == "1":
+#  print("entrez le nom d'un élèment à ajouter à la liste de courses")
+
+# #condition 1
 
