8cd41738388a55f64700f50ac8e0dc76dadf2bca
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-04 15:08:56 +0200

    essayer de trouver le 4

--- coursLesBases/la_liste_des_courses.py
+++ coursLesBases/la_liste_des_courses.py
@@ -15,13 +15,13 @@ votre choix: """
 
 Choix_du_menu = ["1", "2", "3", "4", "5"]
 
- while True:
-     Choix_du_menu = ""
-     while Choix_du_menu not in Menu:
-         Choix_du_menu = input(Menu)
-         if Choix_du_menu not in Menu:
-             print("Veuillez choisir une option valide: ")
-
+while True:
+    Choix_du_menu = ""
+    while Choix_du_menu not in Menu:
+        Choix_du_menu = input(Menu)
+        if Choix_du_menu not in Menu:
+            print("Veuillez choisir une option valide: ")
+             
     if Choix_du_menu == "1": 
         item = input("Entrez le nom de l'élèment à ajouter à la liste de courses : ")
         LISTE.append(item)
@@ -29,29 +29,30 @@ Choix_du_menu = ["1", "2", "3", "4", "5"]
         
     elif Choix_du_menu == "2":
         item = input ("Entrez le nom de l'élèment à retirer de la liste : ")
-        LISTE.remove(item)
-        print(f"L'élèment {item} a bien été retiré de la liste.")
+        #vérifier que l'élèment est bien dans la liste
+        if item in LISTE :
+            LISTE.remove(item)
+            print(f"L'élèment {item} a bien été retiré de la liste.")
+        else : 
+            print(f"L'élèment {item} n'est pas dans la liste.")
         
     elif Choix_du_menu == "3":
-        print(LISTE)
+        #il faut faire une boucle à chaque fois pour vérifier qu'il y ait des élèments dans la liste ou non
+        if LISTE :
+            print("Voici le contenu de votre liste : ")
+            for i, item in enumerate(LISTE, 1):
+                print(f"{i}. {item}")
+        
+        else :
+            print("Votre liste est vide.")
+            
+       
         
     elif Choix_du_menu == "4":
-        clear(LISTE)
-        print("Le contenu de la liste a bien été effacé.")
+        if LISTE == None:
+            print("Votre liste était vide")
+        else :
+            clear(LISTE)
+            print("Le contenu de la liste a bien été effacé.")
     
     else Choix_du_menu == "5":
-        
-    
-
-
-
-# liste_des_courses = []
-
-# # mettre les propositions en ligne 
-
-# input ("choisissez parmi les 5 options suivante : 1 : ajouter un élèment à la liste")
-# if i == "1":
-#  print("entrez le nom d'un élèment à ajouter à la liste de courses")
-
-# #condition 1
-
