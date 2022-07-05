230c654fd353ff9b400eafe12c235211d5119efd
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-05 10:44:16 +0200

    consignes + premières variables

--- coursLesBases/projet_jeux_mystere.py
+++ coursLesBases/projet_jeux_mystere.py
@@ -2,3 +2,17 @@
 
 # titre 
 print("*** Le jeu du nombre mystère***")
+
+# il te reste 5 essais
+#Il te reste {nombre_essai}.
+# devine le nombre
+# Veuillez entrer un nombre valide 
+
+# le nombre mystere est plus petit que {user_choice}
+# le nombre mystere est plus grand que {user_choice}
+#Dommage! Le nombre mystère était {nombre_mystere}
+#Fin du jeu. 
+
+# Bravo! Le nombre mystère était bien {nombre_mystere}!
+# Tu as trouvé le nombre en {nombre essai}
+# Fin du jeu. 
