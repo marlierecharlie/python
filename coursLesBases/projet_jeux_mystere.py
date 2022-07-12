3898a0a58c23c8f2597826d1eacd9911c8bb55bd
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-12 15:50:22 +0200

    input et phrase de fin 1er essai

--- coursLesBases/projet_jeux_mystere.py
+++ coursLesBases/projet_jeux_mystere.py
@@ -1,12 +1,11 @@
+from pickletools import int4
 import random
 # trouver un nombre aléatoire entre 0 et 100
 
 # titre 
-
-
-
 print("*** Le jeu du nombre mystère***")
 
+nombre_essai = int
 # il te reste 5 essais
 #Il te reste {nombre_essai}.
 # devine le nombre
@@ -29,3 +28,21 @@ def selectRandom(Liste):
     return random.choice(Liste)
 
 print(selectRandom(Liste))
+while True:
+    user_choice = ""
+    while user_choice != selectRandom(Liste):
+        user_choice = input("Veuillez entrer un nombre valide : ")
+        # for i in nombre_essai :
+        #     if i <= 5 :
+        #         i+=1
+        #     # nombre_essai(i+=1)
+        #     print(f"il te reste {nombre_essai} essai")
+        # if nombre_essai > 5 :
+        #     break
+        
+    if user_choice == selectRandom(Liste) :
+        print("""
+          # Bravo! Le nombre mystère était bien {nombre_mystere}!
+# Tu as trouvé le nombre en {nombre essai}
+# Fin du jeu.""" )
+    
