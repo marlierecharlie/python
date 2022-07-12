c8e16cb3f326149365c7d79b244803147a9dd23a
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-12 16:54:26 +0200

    code fonctionne partiellement

--- coursLesBases/projet_jeux_mystere.py
+++ coursLesBases/projet_jeux_mystere.py
@@ -8,10 +8,6 @@ user_choice = ""
 # titre 
 print("*** Le jeu du nombre mystère***")
 
-# le nombre mystere est plus petit que {user_choice}
-# le nombre mystere est plus grand que {user_choice}
-
-
 print(number_to_find)
 
 while remaining_attempts > 0 :
@@ -28,11 +24,18 @@ while remaining_attempts > 0 :
     user_choice = int(user_choice)
     
 
-    if user_choice == number_to_find :
+    if user_choice > number_to_find :
+        print(f"le nombre mystere est plus petit que {user_choice}")
+    elif user_choice < number_to_find :
+        print(f"le nombre mystere est plus grand que {user_choice}")
+    else :
         print(f"""Bravo! Le nombre mystère était bien {number_to_find}!
-Tu as trouvé le nombre en {remaining_attempts}
+Tu as trouvé le nombre en {remaining_attempts} essai{'s' if remaining_attempts > 1 else ''}
 """ )
-        
+    
+    remaining_attempts -= 1
+    
+     
 if remaining_attempts == 0 :
     print (f"""
            Dommage! Le nombre mystère était {number_to_find}
