a9535860be9b559f9667a7538530b1ef65446938
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-18 21:26:34 +0200

    le script attribue une potion d'office, fait un duel puis termine le jeu

--- coursLesBases/projet_jeu_de_role.py
+++ coursLesBases/projet_jeu_de_role.py
@@ -6,7 +6,7 @@ my_lifescore = 50
 ennemy_lifescore = 50
 nombre_de_potions = 3
 points_de_potion = random.randint(15,50)
-user_choice = ""
+user_choice = 1 | 2
 
 # chaque potion permet de réccupérer aléatoirement entre 15
 # 50 pts de vie 
@@ -19,18 +19,18 @@ if my_lifescore >= 50:
     if ennemy_lifescore > 0 :
         input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) : ")
         
-        if not user_choice.isdigit():
-            input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) : ")
+        # if not user_choice.isdigit():
+        #     input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) : ")
 
         # permet d'écraser str user choice par un nombre entier    
-        user_choice = int(user_choice)
+        # user_choice = int(user_choice)
         
         if user_choice == 1 :
             print(f"l'ennemi vous a infligé {points_de_degats} points de dégats.")
             print(f"Il vous reste {my_lifescore - points_de_degats} points de vie.")
             print(f"Il reste {ennemy_lifescore - points_de_degats_enemy} points de vie à l'ennemi.")
 
-        if user_choice == 2 :
+        else :
             if nombre_de_potions >= 3 :
                 nombre_de_potions -= 1
                 my_lifescore = my_lifescore + points_de_potion
