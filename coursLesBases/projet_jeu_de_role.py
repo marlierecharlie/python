b145151132dc9ff651b2a27318aebf9a3e0f9b70
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-18 15:09:08 +0200

    1er étape : copier consignes, commencer à définir variables

---
+++ coursLesBases/projet_jeu_de_role.py
@@ -0,0 +1,25 @@
+# un ennemi et moi
+# 50 pts de vie chacun
+# pas de postion pour l"ennemi
+# j'ai 3 potions
+# chaque potion permet de réccupérer aléatooirement entre 15
+# 50 pts de vie 
+# mes attaquent infligent entre 5 et 10 pts de vie à l'ennemi
+# l'ennemi m'inflige entre 5 et 15 pts de visiblename
+# je passe le prochain tour en utilisant une potion
+
+input("Souhaitez-vous attaquer (1) ou utiliser une potion (2)")
+
+print("l'ennemi vous a infligé {points_de_degats} points de dégats.")
+print("Il vous reste {my_lifescore} points de vie.")
+print("Il reste {ennemy_lifescore} points de vie à l'ennemi.")
+
+print("Vous réccupérez {my_life_point} points de vie.")
+print("Il vous reste {potions}.")
+
+print("Vous passez votre tour!")
+
+print("Vous n'avez plus de potions.")
+
+print("Tu as gagné!")
+print("Fin du jeu.")
