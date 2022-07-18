ce656bf2a8df8def5076be8ba471945ce609c22d
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-18 21:52:21 +0200

    toujouts pas

--- coursLesBases/projet_jeu_de_role.py
+++ coursLesBases/projet_jeu_de_role.py
@@ -6,7 +6,7 @@ my_lifescore = 50
 ennemy_lifescore = 50
 nombre_de_potions = 3
 points_de_potion = random.randint(15,50)
-user_choice = [1, 2]
+user_choice = ""
 entre = input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) : ")
 
 # chaque potion permet de réccupérer aléatoirement entre 15
@@ -26,14 +26,13 @@ while my_lifescore > 0:
         # permet d'écraser str user choice par un nombre entier    
         # user_choice = int(user_choice)
         
-        if user_choice == 1 :
+        if user_choice == "1" :
             my_lifescore = my_lifescore - points_de_degats
             print(f"l'ennemi vous a infligé {points_de_degats} points de dégats.")
             print(f"Il vous reste {my_lifescore} points de vie.")
             ennemy_lifescore = ennemy_lifescore - points_de_degats_enemy
             print(f"Il reste {ennemy_lifescore} points de vie à l'ennemi.")
-
-        if user_choice == 2 :
+        # if user_choice == "2" :
             if nombre_de_potions >= 3 :
                 nombre_de_potions -= 1
                 my_lifescore = my_lifescore + points_de_potion
