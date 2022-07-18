2d37952cca80f8baca078a85268d89b254c57467
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-18 15:30:51 +0200

    prémices du script

--- coursLesBases/projet_jeu_de_role.py
+++ coursLesBases/projet_jeu_de_role.py
@@ -1,21 +1,38 @@
-# un ennemi et moi
-# 50 pts de vie chacun
-# pas de postion pour l"ennemi
-# j'ai 3 potions
-# chaque potion permet de réccupérer aléatooirement entre 15
+import random
+from unicodedata import digit
+
+points_de_degats = random.randint(5,15)
+points_de_degats_enemy = random.randint(5,10)
+my_lifescore = 50
+ennemy_lifescore = 50
+nombre_de_potions = 3
+points_de_potion = random.randint(15,50)
+user_choice = ""
+
+# chaque potion permet de réccupérer aléatoirement entre 15
 # 50 pts de vie 
 # mes attaquent infligent entre 5 et 10 pts de vie à l'ennemi
 # l'ennemi m'inflige entre 5 et 15 pts de visiblename
 # je passe le prochain tour en utilisant une potion
 
-input("Souhaitez-vous attaquer (1) ou utiliser une potion (2)")
+while my_lifescore >= 50:
+        input("Souhaitez-vous attaquer (1) ou utiliser une potion (2)")
+        
+        if not user_choice.isdigit():
+            input("Souhaitez-vous attaquer (1) ou utiliser une potion (2)")
+        
+        # permet d'écraser str user choice par un nombre entier    
+        user_choice = int(user_choice)
+        
+        if user_choice == 1 :
+            print(f"l'ennemi vous a infligé {points_de_degats} points de dégats.")
+            print(f"Il vous reste {my_lifescore - points_de_degats} points de vie.")
+            print(f"Il reste {ennemy_lifescore - points_de_degats_enemy} points de vie à l'ennemi.")
 
-print("l'ennemi vous a infligé {points_de_degats} points de dégats.")
-print("Il vous reste {my_lifescore} points de vie.")
-print("Il reste {ennemy_lifescore} points de vie à l'ennemi.")
-
-print("Vous réccupérez {my_life_point} points de vie.")
-print("Il vous reste {potions}.")
+        if user_choice == 2 :
+            nombre_de_potions -= 1
+            print("Vous réccupérez {my_lifescore} points de vie.")
+            print(f"Il vous reste {nombre_de_potions}.")
 
 print("Vous passez votre tour!")
 
