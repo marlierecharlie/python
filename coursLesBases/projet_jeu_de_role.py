c86f440e879988b164323ba65b5c2285a70bddf0
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-18 21:39:51 +0200

    mon script ne tourne plus

--- coursLesBases/projet_jeu_de_role.py
+++ coursLesBases/projet_jeu_de_role.py
@@ -6,7 +6,8 @@ my_lifescore = 50
 ennemy_lifescore = 50
 nombre_de_potions = 3
 points_de_potion = random.randint(15,50)
-user_choice = 1 | 2
+user_choice = [1, 2]
+entre = input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) : ")
 
 # chaque potion permet de réccupérer aléatoirement entre 15
 # 50 pts de vie 
@@ -14,28 +15,30 @@ user_choice = 1 | 2
 # l'ennemi m'inflige entre 5 et 15 pts de visiblename
 # je passe le prochain tour en utilisant une potion
 
-if my_lifescore >= 50:
+while my_lifescore > 0:
     
     if ennemy_lifescore > 0 :
-        input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) : ")
+        entre
         
         # if not user_choice.isdigit():
-        #     input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) : ")
+        #     entre
 
         # permet d'écraser str user choice par un nombre entier    
         # user_choice = int(user_choice)
         
         if user_choice == 1 :
+            my_lifescore = my_lifescore - points_de_degats
             print(f"l'ennemi vous a infligé {points_de_degats} points de dégats.")
-            print(f"Il vous reste {my_lifescore - points_de_degats} points de vie.")
-            print(f"Il reste {ennemy_lifescore - points_de_degats_enemy} points de vie à l'ennemi.")
+            print(f"Il vous reste {my_lifescore} points de vie.")
+            ennemy_lifescore = ennemy_lifescore - points_de_degats_enemy
+            print(f"Il reste {ennemy_lifescore} points de vie à l'ennemi.")
 
-        else :
+        if user_choice == 2 :
             if nombre_de_potions >= 3 :
                 nombre_de_potions -= 1
                 my_lifescore = my_lifescore + points_de_potion
                 print(f"Vous réccupérez {my_lifescore} points de vie.")
-                print(f"Il vous reste {nombre_de_potions}.")
+                print(f"Il vous reste {nombre_de_potions} potions.")
                 print("Vous passez votre tour!")
                 my_lifescore = my_lifescore - points_de_degats
                 print(f"l'ennemi vous a infligé {points_de_degats} points de dégats.")
@@ -44,7 +47,7 @@ if my_lifescore >= 50:
             
             else :
                 print("Vous n'avez plus de potions.")
-                print(user_choice)
+                entre
 
     else :
         print("Tu as gagné!")
