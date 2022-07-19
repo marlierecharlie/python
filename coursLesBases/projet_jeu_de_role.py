221f6430ec7fbe0a2c2b3e5046a81464bf8d04b8
Mcharlie01 <97100761+Mcharlie01@users.noreply.github.com>
2022-07-19 15:22:16 +0200

    correction ok

--- coursLesBases/projet_jeu_de_role.py
+++ coursLesBases/projet_jeu_de_role.py
@@ -6,32 +6,26 @@ my_lifescore = 50
 ennemy_lifescore = 50
 nombre_de_potions = 3
 points_de_potion = random.randint(15,50)
-user_choice = "1" or "2"
-entre = input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) : ")
+# user_choice est une liste de caractères
+user_choice = ""
 
-# chaque potion permet de réccupérer aléatoirement entre 15
-# 50 pts de vie 
-# mes attaquent infligent entre 5 et 10 pts de vie à l'ennemi
-# l'ennemi m'inflige entre 5 et 15 pts de visiblename
-# je passe le prochain tour en utilisant une potion
 
 while my_lifescore > 0:
     
-    if ennemy_lifescore > 0 :
-        entre
+    while ennemy_lifescore > 0 :
         
-        if user_choice is False :
-            entre
-        
+        # while user_choice not in ["1","2"]:
+        #     user_choice = input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) : ")
+       
         if user_choice == "1" :
             my_lifescore = my_lifescore - points_de_degats
             print(f"l'ennemi vous a infligé {points_de_degats} points de dégats.")
             print(f"Il vous reste {my_lifescore} points de vie.")
             ennemy_lifescore = ennemy_lifescore - points_de_degats_enemy
             print(f"Il reste {ennemy_lifescore} points de vie à l'ennemi.")
-            entre
             
-        if user_choice == "2" :
+            
+        elif user_choice == "2" :
             if nombre_de_potions >= 3 :
                 nombre_de_potions -= 1
                 my_lifescore = my_lifescore + points_de_potion
@@ -45,7 +39,7 @@ while my_lifescore > 0:
             
             else :
                 print("Vous n'avez plus de potions.")
-                entre
+                input("Souhaitez-vous attaquer (1) ou utiliser une potion (2) : ")
 
     else :
         print("Tu as gagné!")
