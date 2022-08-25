7f1866878ea27caf9bf8d6423263f5342b0bbb50
marlierecharlie <97100761+Mcharlie01@users.noreply.github.com>
2022-08-25 16:21:20 +0200

    exercice avec augustin

---
+++ coursLesBases/les_fonctions/fizzbuzz.py
@@ -0,0 +1,15 @@
+# le principe du fizzbuzz est sur des nombres de 1 à 100 afficher fizz si il est divisible par 3, buzz si il l'est par 5 et fizzbuzz si
+# il est divisible par les deux, sinon afficher le nombre tel quel.
+
+# utiliser plusieurs fois if permet de rentrer dans toutes le conditions, mais alourdi le programme 
+# utiliser les elif permet à la boucle de passer à la suivante que si celle-ci n'est pas validée.
+
+# for i in range(101):
+#     if i%15==0
+#         print("fizzbuzz")
+#     elif i%5==0:
+#         print("buzz")
+#     elif i%3==0:
+#         print("fizz")
+#     else:
+#         print(i)
